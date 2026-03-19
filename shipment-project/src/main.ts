da872775d299e27a5979f86ab1afa6e48f204e30
tamar <tamar21487@gmail.com>
2026-03-19 17:13:08 +0200

    connect to kafka

--- shipment-project/src/main.ts
+++ shipment-project/src/main.ts
@@ -1,10 +1,24 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
 import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
- 
+
+  app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.KAFKA,
+    options: {
+      client: {
+        brokers: ['localhost:9092'],
+      },
+      consumer: {
+        groupId: 'shipment-consumer-group',
+      },
+      subscribe: {
+        fromBeginning: true
+      },
+    },
+  })
 
   await app.startAllMicroservices();
   await app.listen(process.env.PORT ?? 3000);
