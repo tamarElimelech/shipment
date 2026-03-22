4b5520e5e945889d2e9ab87fb2ca2a94c9fc40e8
tamar <tamar21487@gmail.com>
2026-03-22 13:46:56 +0200

    add configuration client to producer

--- shipment-project/src/shipment-status/shipment-status.module.ts
+++ shipment-project/src/shipment-status/shipment-status.module.ts
@@ -1,12 +1,25 @@
 import { Module } from '@nestjs/common';
-import { ShipmentStatusService } from './shipment-status.service';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+import { ShipmentStatusConsumer } from './kafka/shipment.consumer';
 import { ShipmentStatusController } from './shipment-status.controller';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { ShipmentStatus } from './entities/shipment-status.entity';
+import { ShipmentStatusService } from './shipment-status.service';
 
 @Module({
-  imports:[TypeOrmModule.forFeature([ShipmentStatus])],
-  controllers: [ShipmentStatusController],
+  imports: [ClientsModule.register([
+    {
+      name: 'notification-client',
+      transport: Transport.KAFKA,
+      options: {
+        client: {
+          clientId: 'notifications-producer',
+          brokers: ['localhost:9092'],
+        }
+      },
+    }
+  ])],
+  controllers: [
+    ShipmentStatusController,
+    ShipmentStatusConsumer],
   providers: [ShipmentStatusService],
 })
-export class ShipmentStatusModule {}
+export class ShipmentStatusModule { }
