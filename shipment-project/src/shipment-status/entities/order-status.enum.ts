175f1993b043abe22f13e91224c6b4029281b78a
tamar <tamar21487@gmail.com>
2026-03-22 13:42:39 +0200

    order status enum

--- shipment-project/src/shipment-status/entities/order-status.enum.ts
+++ shipment-project/src/shipment-status/entities/order-status.enum.ts
@@ -1,6 +1,6 @@
 export enum orderStatus {
-    Created,
-    Packed,
-    Shipped,
-    Delivered
+    Created = 'Created',
+    Packed = 'Packed',
+    Shipped = 'Shipped',
+    Delivered = 'Delivered'
 }
