fb5e08f24560c8aa38ea513d4293e40696a3f5ad
tamar <tamar21487@gmail.com>
2026-03-22 13:42:17 +0200

    shipment-status dto

--- shipment-project/src/shipment-status/dto/create-shipment-status.dto.ts
+++ shipment-project/src/shipment-status/dto/create-shipment-status.dto.ts
@@ -1,1 +1,12 @@
-export class CreateShipmentStatusDto {}
+import { IsEnum, IsNotEmpty, IsNumber } from "class-validator";
+import { orderStatus } from "../entities/order-status.enum";
+
+export class CreateShipmentStatusDto {
+
+    @IsNumber()
+    @IsNotEmpty()
+    orderId: number
+
+    @IsEnum(orderStatus)
+    status: orderStatus
+}
--- shipment-project/src/shipment-status/dto/update-shipment-status.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateShipmentStatusDto } from './create-shipment-status.dto';
-
-export class UpdateShipmentStatusDto extends PartialType(CreateShipmentStatusDto) {}
