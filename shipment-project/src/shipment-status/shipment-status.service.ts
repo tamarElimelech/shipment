c5f8edc1d7068ac1ba07b74f7677fcf58aaaff2d
tamar <tamar21487@gmail.com>
2026-03-22 13:49:49 +0200

    add handleNewStatus
    get message from kafka, and add to db

--- shipment-project/src/shipment-status/shipment-status.service.ts
+++ shipment-project/src/shipment-status/shipment-status.service.ts
@@ -1,26 +1,43 @@
-import { Injectable } from '@nestjs/common';
-import { CreateShipmentStatusDto } from './dto/create-shipment-status.dto';
-import { UpdateShipmentStatusDto } from './dto/update-shipment-status.dto';
-
+import { Inject, Injectable, OnModuleInit } from '@nestjs/common';
+import { ClientKafka } from '@nestjs/microservices';
+import * as sql from 'mssql';
 @Injectable()
-export class ShipmentStatusService {
-  create(createShipmentStatusDto: CreateShipmentStatusDto) {
-    return 'This action adds a new shipmentStatus';
+export class ShipmentStatusService implements OnModuleInit {
+  constructor(
+    @Inject('mssql_pool') private readonly pool: sql.ConnectionPool,
+    @Inject('notification-client') private readonly kafkaClient: ClientKafka,
+  ) { }
+  async onModuleInit() {
+    await this.kafkaClient.connect()
   }
 
-  findAll() {
-    return `This action returns all shipmentStatus`;
+  async handleNewStatus(orderId: number, status: string) {
+    try {
+
+      const checkRequest = await this.pool.request()
+        .input('orderId', sql.Int, orderId)
+        .input('status', sql.NVarChar, status)
+        .query('select id from dbo.shipment_status where order_id=@orderId and status=@status')
+
+      if (checkRequest.recordset.length > 0) {
+        console.log(`Status ${status} for Order ${orderId} already exists. Skipping...`)
+        return
+      }
+
+      const insertRequest = await this.pool.request()
+        .input('orderId', sql.Int, orderId)
+        .input('status', sql.NVarChar, status)
+        .query(`insert into dbo.shipment_status (order_id, status, event_time ) 
+            output inserted.id, inserted.status, inserted.event_time
+            values (@orderId, @status, getdate())`)
+      const newRow = insertRequest.recordset[0]
+
+
+      return newRow
+    } catch (err) {
+      console.error(`[ShipmentStatusService] can't insert to db: ${err.message}`)
+    }
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} shipmentStatus`;
-  }
 
-  update(id: number, updateShipmentStatusDto: UpdateShipmentStatusDto) {
-    return `This action updates a #${id} shipmentStatus`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} shipmentStatus`;
-  }
 }
