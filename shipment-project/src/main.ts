97f9f10aa52877f6d08e2c98a7ebcf992a511d9c
tamar <tamar21487@gmail.com>
2026-03-22 15:01:23 +0200

    add swagger config

--- shipment-project/src/main.ts
+++ shipment-project/src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -21,6 +22,12 @@ async function bootstrap() {
   })
 
   await app.startAllMicroservices()
+  const options = new DocumentBuilder()
+    .setTitle('shipment')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
 
 
   await app.listen(process.env.PORT ?? 3000);
